bf4f99665f31ff2641283efd83408e3f5bd60322
Vincent Servio <vincentservio@gmail.com>
2020-02-12 19:55:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 require "pry"
 def reverse_each_word(sentence)
   string = sentence.split(" ")
+  new_string = string.collect{|i| i.reverse}
+new_string.to_s
+
   binding.pry
 end
 
-new_string = string.collect{|i| i.reverse}
-new_string.to_s
