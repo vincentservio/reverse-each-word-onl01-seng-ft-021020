30789bd99fdb72833cf62ee1fa4e9ccfe9a11c38
Vincent Servio <vincentservio@gmail.com>
2020-02-12 20:24:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 def reverse_each_word(sentence)
   string = sentence.split(" ")
   new_string = string.collect{|i| i.reverse}
-new_string.join(",")
+new_string.join(" ")
 
   #binding.pry
 end
